750a4af00c613d27fd7dec4dd9d49e6384ea055c
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-05-26 23:17:58 +0600

    something updated

--- index.js
+++ index.js
@@ -16,33 +16,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.csluy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = 'mongodb+srv://machinedb:KLxxBmspoMOszy09@cluster0.csluy.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-function  verifyJWT(req, res, next){
-  const authHeader = req.headers.authorization;
-  if(!authHeader){
-      return res.status(401).send({message: 'UnAthorized access'})
-  }
-  const token = authHeader.split(' ')[1];
- 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-      console.log('decoded',decoded)
-      if(err){
-          
-      return  res.status(403).send({message: "Forbidden access"})
-      }
-      req.decoded = decoded;
-      // console.log(req.decoded)
-      next()
-    });
-  }
-
-
-async function run() {
-  try {
-    await client.connect();
     const partsCollection = client.db('allParts').collection('parts');
     const userCollection = client.db('allParts').collection('users');
     const orderCollection = client.db('allParts').collection('order');
